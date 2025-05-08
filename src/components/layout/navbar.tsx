16bbea6cfe0d0f6606ca1f9e55b577db261694d7
abidgymnastiar <abidgymnastiar@gmail.com>
2025-05-08 14:35:59 +0700

    add : slicing navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -3,35 +3,39 @@ import { Button } from "../ui/button";
 
 export default function Navbar() {
   return (
-    <div className="">
-      <nav className="flex items-center justify-between p-4 text-white">
-        <div>
-          <Image src="/Logo.svg" alt="Logo" width={92} height={24} />
+    <nav className="py-6 fixed top-0 w-full">
+      <div className="w-full max-w-[1072px] mx-auto ">
+        <div className="flex items-center justify-between gap-10">
+          <div>
+            <Image src="/Logo.svg" alt="Logo" width={92} height={24} />
+          </div>
+          <ul className="flex flex-1 justify-start gap-2">
+            <li className="px-3 py-2">
+              <a href="#" className="text-grayscale-950">
+                About
+              </a>
+            </li>
+            <li className="px-3 py-2">
+              <a href="/about" className="text-grayscale-950 ">
+                Pricing
+              </a>
+            </li>
+            <li className="px-3 py-2">
+              <a href="/contact" className="text-grayscale-950">
+                Blog
+              </a>
+            </li>
+          </ul>
+          <div className="flex gap-3">
+            <Button variant="Secondary" size="small">
+              Contact Sales
+            </Button>
+            <Button variant="primary" size="small">
+              Start for Free
+            </Button>
+          </div>
         </div>
-        <ul className="flex space-x-4">
-          <li>
-            <a href="#" className="text-grayscale-950">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="/about" className="text-grayscale-950 ">
-              Pricing
-            </a>
-          </li>
-          <li>
-            <a href="/contact" className="text-grayscale-950">
-              Blog
-            </a>
-          </li>
-        </ul>
-        <div className="">
-          <Button variant="primary" size="base">Button</Button>
-          <Button variant="primary" size="small">Button</Button>
-          <Button variant="Secondary" size="small">Button</Button>
-          <Button variant="primary" size="small">Button</Button>
-        </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
